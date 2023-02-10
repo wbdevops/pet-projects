56de9a7f2caef7c36e8c3a2753bcd8af0fa15ac8
Oleksandr Kyktenko <o.kyktenko@megogo.net>
2023-02-10 17:12:00 +0100

    minor fix

--- proxmox-tf/provider.tf
+++ proxmox-tf/provider.tf
@@ -1,8 +1,8 @@
 terraform {
     required_providers {
         proxmox = {
-        source = "telmate/proxmox"
-        version = "2.9.11"
+            source = "telmate/proxmox"
+            version = "2.9.11"
         }
     }
 }
